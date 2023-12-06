5033c3ab6bcaf0b37df8da7f5cb01aaf7b8147b5
rakiz <github@rakiz.net>
2023-12-06 13:52:23 +0100

    Day 6

---
+++ Day06/Day06.py
@@ -0,0 +1,59 @@
+import math
+import time
+
+INPUT_FILE = "input.txt"
+TEST_FILE = "test.txt"
+epsilon = 0.00000001
+
+
+# returns [(time,distance to beat),...]
+def get_data(filename, is_part1):
+    races = []
+    with open(filename, 'r', encoding='UTF-8') as file:
+        # Times
+        _, *times = file.readline().rstrip().split()
+        _, *distances = file.readline().rstrip().split()
+        if is_part1:
+            for i in range(len(times)):
+                races.append((int(times[i]), int(distances[i])))
+        else:
+            race_time = ""
+            distance = ""
+            for i in range(len(times)):
+                race_time += times[i]
+                distance += distances[i]
+            races.append((int(race_time), int(distance)))
+
+    return races
+
+
+def execute(races):
+    print("Start - races: {}".format(races))
+    t0 = time.time_ns()
+    result = 1  # not 0, we're multiplying here !
+    for race_time, distance in races:
+        # x is the race_time we press the button
+        # distance < (race_time - x) * x  =>  distance < race_time*x - x*x
+        # =>   x² - race_time*x + distance < 0  (ax²+bx+c=0)
+        a, b, c = 1, -race_time, distance
+        # √D = √(race_time²−4*distance)    √(b²-4ac)
+        racine_delta = math.sqrt(b * b - 4 * a * c)
+        # solutions: (-race_time +/- √D) / 2         (-b +/- √D) / 2a
+        # trick: add epsilon value and ceil min solution; sub epsilon value and floor max solution
+        solution1 = math.ceil(((-b - racine_delta) / 2) + epsilon)
+        solution2 = math.floor(((-b + racine_delta) / 2) - epsilon)
+        # multiplying with the number of ways to beat it
+        result *= solution2 - solution1 + 1
+
+    t1 = time.time_ns()
+    print("End ({}ns)- result = {}\n".format((t1 - t0), result))
+
+    return result
+
+
+if __name__ == '__main__':
+    assert execute(get_data(TEST_FILE, True)) == 288
+    execute(get_data(INPUT_FILE, True))  # 114400
+
+    assert execute(get_data(TEST_FILE, False)) == 71503
+    execute(get_data(INPUT_FILE, False))  # 21039729
---
+++ Day06/README.md
@@ -0,0 +1,11 @@
+Start - races: [(7, 9), (15, 40), (30, 200)]\
+End (0ns)- result = 288
+
+Start - races: [(35, 212), (93, 2060), (73, 1201), (66, 1044)]\
+End (0ns)- result = 114400
+
+Start - races: [(71530, 940200)]\
+End (0ns)- result = 71503
+
+Start - races: [(35937366, 212206012011044)]\
+End (0ns)- result = 21039729
